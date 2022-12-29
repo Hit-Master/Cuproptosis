582f0dcc9cd3a5654ef3d9f348cfce15329e7cba
Hit-Master <121613716+Hit-Master@users.noreply.github.com>
2022-12-29 22:00:01 +0800

    Update step03annotation.R

--- Single-cell analysis/step03annotation.R
+++ Single-cell analysis/step03annotation.R
@@ -1,41 +1,84 @@
-seu_obj=readRDS('scRNA_main_annotated.RDS')
-metatable <- read_excel("./metadata/patients_metadata.xlsx")
+library(Seurat)
+library(dplyr)
+library(reticulate)
+library(sctransform)
+library(cowplot)
+library(ggplot2)
+library(viridis)
+library(tidyr)
+library(magrittr)
+library(reshape2)
+library(readxl)
+library(readr)
+library(stringr)
+library(progeny)
+library(scales)
 
-metadata <- FetchData(seu_obj, "orig.ident")
-metadata$cell_id <- rownames(metadata)
-metadata$sample_id <- metadata$orig.ident
+theme_set(theme_cowplot())
 
-metadata <- left_join(x = metadata, y = metatable, by = "sample_id")
+use_colors <- c(
+  Tumor = "brown2",
+  Normal = "deepskyblue2",
+  G1 = "#46ACC8",
+  G2M = "#E58601",
+  S = "#B40F20",
+  Epithelial = "seagreen",
+  Immune = "darkgoldenrod2",
+  Stromal = "steelblue",
+  p018 = "#E2D200",
+  p019 = "#46ACC8",
+  p023 = "#E58601",
+  p024 = "#B40F20",
+  p027 = "#0B775E",
+  p028 = "#E1BD6D",
+  p029 = "#35274A",
+  p030 = "#F2300F",
+  p031 = "#7294D4",
+  p032 = "#5B1A18",
+  p033 = "#9C964A",
+  p034 = "#FD6467")
 
-rownames(metadata) <- metadata$cell_id
+epi <- readRDS("seurat_objects/epi.RDS")
+imm <- readRDS("seurat_objects/imm.RDS")
+str <- readRDS("seurat_objects/str.RDS")
 
+### epithelial subclustering
+epi <- RunPCA(epi)
+ElbowPlot(epi,  ndims = 50)
 
-seu_obj <- AddMetaData(seu_obj, metadata = metadata)
-
-s.genes <- cc.genes$s.genes
-g2m.genes <- cc.genes$g2m.genes
-
-score_cc <- function(seu_obj) {
-  seu_obj <- CellCycleScoring(seu_obj, s.genes, g2m.genes)
-  seu_obj@meta.data$CC.Diff <- seu_obj@meta.data$S.Score - seu_obj@meta.data$G2M.Score
-  return(seu_obj)
+epi <- RunUMAP(epi, dims = 1:20)
+epi <- FindNeighbors(epi, dims = 1:20)
+for (i in c(0.2, 0.3, 0.4, 0.5, 1, 2)) {
+  epi <- FindClusters(epi, resolution = i)
+  print(DimPlot(epi, reduction = "umap", label = T) + labs(title = paste0("resolution: ", i)))
 }
 
-seu_obj <- score_cc(seu_obj)
- 
-FeatureScatter(seu_obj, "G2M.Score", "S.Score", group.by = "Phase", pt.size = .1) +
-  coord_fixed(ratio = 1)
+Idents(epi) <- epi@meta.data$SCT_snn_res.1
 
 
-Idents(seu_obj) <- seu_obj@meta.data$Main_cell_type
-epi <- subset(seu_obj, idents = "Epithelial")
-imm <- subset(seu_obj, idents = "Immune")
-str <- subset(seu_obj, idents = "Stromal")
+### immune subclustering
+imm <- RunPCA(imm)
+ElbowPlot(imm,  ndims = 50)
 
-epi <- ScaleData(epi)
-imm <- ScaleData(imm)
-str <- ScaleData(str)
+imm <- RunUMAP(imm, dims = 1:20)
+imm <- FindNeighbors(imm, dims = 1:20)
+for (i in c(0.2, 0.3, 0.4, 0.5, 1, 2)) {
+  imm <- FindClusters(imm, resolution = i)
+  print(DimPlot(imm, reduction = "umap") + labs(title = paste0("resolution: ", i)))
+}
 
-saveRDS(epi, file = "epi.RDS")
-saveRDS(imm, file = "imm.RDS")
-saveRDS(str, file = "str.RDS")
+Idents(imm) <- imm@meta.data$SCT_snn_res.0.5
+
+
+### stromal sublustering
+str <- RunPCA(str)
+ElbowPlot(str, ndims = 50)
+
+str <- RunUMAP(str, dims = 1:20)
+str <- FindNeighbors(str, dims = 1:20)
+for (i in c(0.2, 0.3, 0.4, 0.5, 1, 2)) {
+  str <- FindClusters(str, resolution = i)
+  print(DimPlot(str, reduction = "umap") + labs(title = paste0("resolution: ", i)))
+}
+
+Idents(str) <- str@meta.data$SCT_snn_res.1
